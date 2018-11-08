de650014be71152fda302e2383a570158f5268c6
Lawrence Xu <lxu@yext.com>
2018-11-08 14:32:07 -0500

    Added logic for deleting blacklisted restaurants

--- js/background.js
+++ js/background.js
@@ -5,7 +5,16 @@
 'use strict';
 
 chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({blacklist: []}, function() {
+  chrome.storage.sync.set({blacklist: ["251 Ginza Sushi", "44th Street Minar"]}, function() {
     console.log("No restaurants on the blacklist.");
   });
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    chrome.declarativeContent.onPageChanged.addRules([{
+      conditions: [new chrome.declarativeContent.PageStateMatcher({
+        pageUrl: {hostEquals: 'www.seamless.com'},
+      })
+      ],
+          actions: [new chrome.declarativeContent.ShowPageAction()]
+    }]);
+   });
 });
---
+++ js/runBlacklist.js
@@ -0,0 +1,19 @@
+let restaurants = document.getElementsByClassName("menuplus");
+
+for (i = 0; i < restaurants.length; i++) {
+  let restaurant = restaurants[i];
+  chrome.storage.sync.get(
+    ['blacklist'],
+    function(result) {
+    let blacklist = result['blacklist']
+      for (j = 0; j < blacklist.length; j++) {
+        if (restaurant.innerHTML.search(blacklist[j]) >= 0) {
+            let parent = restaurant.parentNode;
+            console.log(restaurant.innerHTML);
+            parent.removeChild(restaurant);
+          }
+      }
+    }
+  );
+}
+
